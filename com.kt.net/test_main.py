045263e2d50e13e9ec959a5001faf230c4957240
Ariel <khy4701@nate.com>
2017-10-24 14:58:32 +0900

    "Flask Add"

--- com.kt.net/test_main.py
+++ com.kt.net/test_main.py
@@ -1,4 +1,28 @@
+from flask import Flask
+from flask_restful import Api
+
 from PLTEConnector import PLTEConnector
+from ServiceClass import Departments_Meta, Departments_Meta2
+from ServiceClass2 import Departmental_Salary
+
+#Create a engine for connecting to SQLite3.
+app = Flask(__name__)
+api = Api(app)
 
 PLTEConnector.getInstance()
 PLTEConnector.getInstance().sendMessage("Command",2)
+
+ 
+api.add_resource(Departmental_Salary, '/dept/<string:department_name>')
+api.add_resource(Departments_Meta, '/departments/<string:department_name>/<int:abc>')
+api.add_resource(Departments_Meta2,'/departments/')
+
+# def http_error_handler(error):
+#     #return flask.render_template('error.html', error=error), error.code
+#     return error, error.code
+# 
+# for error in (401, 404, 500): # or with other http code you consider as error
+#     app.error_handler_spec[None][error] = http_error_handler
+
+if __name__ == '__main__':
+    app.run(threaded=True)
