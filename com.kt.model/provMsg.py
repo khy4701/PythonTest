eca1477fa1cec308c52188c24a05dbf84c5c0ffb
khy4701 <khy4701@nate.com>
2017-10-24 21:35:13 +0900

    Communicate 1024-2135

--- com.kt.model/provMsg.py
+++ com.kt.model/provMsg.py
@@ -1,6 +1,6 @@
 
 from _ctypes import Structure
-from ctypes import c_int, c_ubyte, c_char, c_short
+from ctypes import c_int, c_ubyte, c_char, c_short, c_long
 
 HTTPF_MSG_BUFSIZE = 1024
 
@@ -14,21 +14,45 @@ class provMsg(Structure):
 
 
 
+# 4 * 5 = 20 (char -- 4bytes) 
 class HttpHeader(Structure):
-    _fields = [("method", c_int),
+    # Serialization ( if don't put this field, it'll calculate 4 byte each other )
+    # But, this is not nessary under communication with C Messaage queue.
+    #_pack_ = 1
+    _fields_ = [("method", c_int),
                ("api_type", c_int),
                ("op_type", c_int),
                ("length", c_int),
                ("encoding", c_char ) ]
 
-
+# 1024 + 8 + 4*5 + 20 =  1072
 class HttpReq(Structure):
-    _fields = [("tot_len", c_int),
-               ("msgId", c_int),
-               ("ehttpf_idx", c_short),
-               ("srcQid", c_int),
-               ("srcSysId", c_char ),
-               ("http_hdr", HttpHeader),
-               ("jsonBody", c_char * HTTPF_MSG_BUFSIZE ) ]
-    
-    
+
+    MTYPE_APP_TO_RESTIF_REQ = 100
+    MTYPE_RESTIF_TO_APP_REQ = 101
+    # Serialization ( if don't put this field, it'll calculate 4 byte each other )
+#    _pack_ = 1
+    _fields_ = [("tot_len", c_int),      # 4
+                ("msgId", c_int),        # 4
+                ("ehttpf_idx", c_short), # 2
+                ("srcQid", c_int),       # 4
+                ("srcSysId", c_char ),   # 1
+                ("http_hdr", HttpHeader),  # 17
+                ("jsonBody", c_char * HTTPF_MSG_BUFSIZE ) ]  # 1024
+
+
+class HttpRes(Structure):
+
+    MTYPE_APP_TO_RESTIF_RES = 102
+    MTYPE_RESTIF_TO_APP_RES = 103
+    # Serialization ( if don't put this field, it'll calculate 4 byte each other )
+#    _pack_ = 1
+    _fields_ = [("tot_len", c_int),      # 4
+                ("msgId", c_int),        # 4
+                ("ehttpf_idx", c_short), # 2
+                ("srcQid", c_int),       # 4
+                ("srcSysId", c_char ),   # 1
+                ("nResult", c_int),       # 4
+                ("http_hdr", HttpHeader),  # 17
+                ("jsonBody", c_char * HTTPF_MSG_BUFSIZE ) ]  # 1024
+
