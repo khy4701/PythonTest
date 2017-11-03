a6a27881bc84228a476e93eb7aefcfdd3e5c1c4b
e2e <e2e@localhost.localdomain>
2017-11-03 13:45:56 +0900

    1103

--- apply.sh
+++ apply.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
 
-git add --all | echo
+if [ $# -lt 1 ]; then
+    echo "$0 Must Input Commit message"
+    exit 0
+fi
+
+result= git add --all | echo
+echo $result
+echo "Add finish.."
 
 sleep 1
-git commit -m $1 | echo
+res= git commit -m $1 | echo
+
+echo $res
 
 
 sleep 1
