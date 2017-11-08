0667901f5e04664040f14638f0f3490153bb3063
Ariel <khy4701@nate.com>
2017-11-08 19:40:31 +0900

    "1108-1940"

--- com.kt.net/PLTEConnector.py
+++ com.kt.net/PLTEConnector.py
@@ -37,6 +37,7 @@ class PLTEConnector(Connector):
                 self.logger.error("msgQueue Connection Failed.. PLTE QUEUE_ID[%d] SIZE[%s]" % (self.plteQueId, maxQSize))
 
 
+    # NBI, SBI
     def sendMessage(self, apiName, httpReqMsg):
         
         self.logger.info("Send Message..!")
@@ -61,7 +62,8 @@ class PLTEConnector(Connector):
             self.logger.info("===============================================")
             self.logger.info("NBRESTIF -> SLEE")
             self.logger.info("===============================================")
-            self.logger.info("gen_mType : " + str(MTYPE_NBRESTIF_TO_SLEE_REQ))
+            self.logger.info("API_NAME : " + str(apiName))
+            self.logger.info("mType : " + str(MTYPE_NBRESTIF_TO_SLEE_REQ))
             self.logger.info("totlen  : "+ str(httpReqMsg.tot_len))
             self.logger.info("msgId   : "+ str(httpReqMsg.msgId))
             self.logger.info("ehttp_idex   : "+ str(httpReqMsg.ehttpf_idx))
@@ -75,12 +77,17 @@ class PLTEConnector(Connector):
             self.logger.info("header.resource_type : "+ str(header.resource_type))
             self.logger.info("header.length : "+ str(header.length))
             self.logger.info("header.encoding : "+ str(header.encoding))
+            self.logger.info("INFO ----------------------------------------")
+            self.logger.info("info.ns_instance_id : "+ str(info.ns_instance_id))
+            self.logger.info("info.nfvo_ip : "+ str(info.nfvo_ip))
+            self.logger.info("info.nfvo_port : "+ str(info.nfvo_port))
             self.logger.info("===============================================")
             
         return True
 
 
-    def sendResMessage(self, command, resMsg):
+    # SBI
+    def sendResMessage(self, apiName, resMsg):
                 
         #GenQMsg = GeneralQResMsg()        
         #GenQMsg.body = resMsg
@@ -96,14 +103,29 @@ class PLTEConnector(Connector):
             self.logger.error("sendMessage Error! %s" % e)
             return False
 
+        header = resMsg.http_hdr
+        
         if ConfManager.getInstance().getLogFlag():
             self.logger.info("===============================================")
             self.logger.info("SBRESTIF -> SLEE")
             self.logger.info("===============================================")
-            self.logger.info("API_NAME : " + str(command))
-            self.logger.info("PID   : "+ str(resMsg.tid))
+            self.logger.info("API_NAME : " + str(apiName))
+            self.logger.info("mType : " + str(MTYPE_SBRESTIF_TO_SLEE_RES))
+            self.logger.info("totlen  : "+ str(resMsg.tot_len))
+            self.logger.info("msgId   : "+ str(resMsg.msgId))
+            self.logger.info("ehttp_idex   : "+ str(resMsg.ehttpf_idx))
+            self.logger.info("tid   : "+ str(resMsg.tid))
+            self.logger.info("srcQid  : "+ str(resMsg.srcQid))
+            self.logger.info("srcSysId: "+ str(resMsg.srcSysId))
             self.logger.info("RESTCODE : " + str(resMsg.nResult) )
             self.logger.info("BODY   : " + resMsg.jsonBody)
+            self.logger.info("HEADER----------------------------------------")
+            self.logger.info("header.method   : "+ str(header.method))
+            self.logger.info("header.api_type : "+ str(header.api_type))
+            self.logger.info("header.op_type  : "+ str(header.op_type))
+            self.logger.info("header.resource_type : "+ str(header.resource_type))
+            self.logger.info("header.length : "+ str(header.length))
+            self.logger.info("header.encoding : "+ str(header.encoding))
             self.logger.info("===============================================")
             
         return True
