ae30041b1c91022ccd0aee81b32f5d978afb53c5
Hoyoung <khy4701@nate.com>
2018-01-30 12:09:56 +0900

    TEST1209

--- Python2_Test/Authentication/Authentication.py
+++ Python2_Test/Authentication/Authentication.py
@@ -2,6 +2,8 @@ CLIENT_ID = "p-jcoLKBynTLew"
 CLIENT_SECRET = "gko_LXELoV07ZBNUXrvWZfzE3aI"
 REDIRECT_URI = "http://localhost:65010/reddit_callback"
 
+test
+
 from flask import Flask
 app = Flask(__name__)
 
